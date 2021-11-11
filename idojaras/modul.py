d949da8528026200fb618fa66595d1baba464efd
PiciAkk <marci@dbx.hu>
2021-11-11 19:50:32 +0100

    weather package added

---
+++ idojaras/modul.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+def idojaras(city):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=1a9a4d9f4f8f8a9a8d9f7c1d0c0e8c8f"
+    return json.dumps(requests.get(url).text)
+
+parancs = self.parancs
+if "időjárás" in parancs.lower():
+    print(idojaras(city))
