264845a65a0c57f47893417fd89d8ef4e4e59ce0
PiciAkk <marci@dbx.hu>
2021-11-11 20:14:42 +0100

    quit() added again

--- base/modul.py
+++ base/modul.py
@@ -1,3 +1,4 @@
 parancs = self.parancs
 if "szia" in parancs.lower() or "hello" in parancs.lower() or "köszöntelek" in parancs.lower():
     self.beszed("Köszöntelek! ROBIN vagyok!")
+    quit()
--- idojaras/modul.py
+++ idojaras/modul.py
@@ -2,3 +2,4 @@ parancs = self.parancs
 if "időjárás" in parancs.lower():
     apikey = "bc12083e70d2d22298c2df1cec7101d9"
     print(idojaras("Budapest"))
+    quit()
