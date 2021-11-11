863143c6b5d51c357e7b75d5d75b62e42880b24e
PiciAkk <marci@dbx.hu>
2021-11-11 20:06:13 +0100

    update

--- idojaras/modul.py
+++ idojaras/modul.py
@@ -1,10 +1,4 @@
-import requests
-import json
-
-def idojaras(city):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=1a9a4d9f4f8f8a9a8d9f7c1d0c0e8c8f"
-    return json.dumps(requests.get(url).text)
-
 parancs = self.parancs
 if "időjárás" in parancs.lower():
+    apikey = "bc12083e70d2d22298c2df1cec7101d9"
     print(idojaras("Budapest"))
