20e8b60b5be64e3c06587724ed574565e85f48a4
PiciAkk <marci@dbx.hu>
2021-11-11 20:28:08 +0100

    Update modul.py

--- idojaras/modul.py
+++ idojaras/modul.py
@@ -1,5 +1,10 @@
 parancs = self.parancs
 if "időjárás" in parancs.lower():
-    apikey = "bc12083e70d2d22298c2df1cec7101d9"
-    print(idojaras("Budapest"))
+    owm = pyowm.OWM("bc12083e70d2d22298c2df1cec7101d9")
+    mgr = owm.weather_manager()
+
+    observation = mgr.weather_at_place("Hungary, Budapest")
+    w = observation.weather
+
+    self.beszed(w.temperature("celsius")["temp"])
     quit()
