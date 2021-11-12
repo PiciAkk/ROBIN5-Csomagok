d35359c76abfcc548314c01bdbebc846dc5f3ad4
piciakk <marci@dbx.hu>
2021-11-12 16:27:22 +0100

    base package is now called alap

---
+++ alap/modul.py
@@ -0,0 +1,5 @@
+parancs = self.parancs
+beszed = self.beszed
+if "szia" in parancs.lower() or "hello" in parancs.lower() or "köszöntelek" in parancs.lower() or "helló" in parancs.lower():
+    beszed("Köszöntelek! ROBIN vagyok!")
+    quit()
--- idojaras/modul.py
+++ idojaras/modul.py
@@ -1,4 +1,5 @@
 parancs = self.parancs
+beszed = self.beszed
 if "időjárás" in parancs.lower():
     owm = pyowm.OWM("bc12083e70d2d22298c2df1cec7101d9")
     mgr = owm.weather_manager()
@@ -6,5 +7,5 @@ if "időjárás" in parancs.lower():
     observation = mgr.weather_at_place("Hungary, Budapest")
     w = observation.weather
 
-    self.beszed(w.temperature("celsius")["temp"])
+    beszed(w.temperature("celsius")["temp"])
     quit()
