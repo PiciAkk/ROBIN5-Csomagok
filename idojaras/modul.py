379fe20340408ac562293c2dcf7ee178b213cf06
piciakk <marci@dbx.hu>
2021-11-12 18:45:01 +0100

    Update modul.py

--- idojaras/modul.py
+++ idojaras/modul.py
@@ -1,6 +1,11 @@
 import pyowm
 import pyowm.utils
 import json
+import requests
+
+def getCity():
+    data = json.loads(requests.get("http://ipinfo.io/json").text)
+    return data["city"]
 
 parancs = self.parancs
 beszed = self.beszed
@@ -8,7 +13,7 @@ if "időjárás" in parancs.lower():
     owm = pyowm.OWM("bc12083e70d2d22298c2df1cec7101d9")
     mgr = owm.weather_manager()
 
-    observation = mgr.weather_at_place("Hungary, Budapest")
+    observation = mgr.weather_at_place(getCity())
     w = observation.weather
     temperature = w.temperature('celsius')['temp']
 
