b1e23a7ecf9715b3e34ed1987b0a12b543cfac3c
piciakk <marci@dbx.hu>
2021-11-12 17:35:07 +0100

    update

--- idojaras/modul.py
+++ idojaras/modul.py
@@ -2,8 +2,8 @@ import pyowm
 import pyowm.utils
 import json
 
-parancs = ROBIN().parancs
-beszed = ROBIN().beszed
+parancs = self.parancs
+beszed = self.beszed
 if "időjárás" in parancs.lower():
     owm = pyowm.OWM("bc12083e70d2d22298c2df1cec7101d9")
     mgr = owm.weather_manager()
