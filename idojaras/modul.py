e28c13741ed16c34281e25b445ad1c8c52731c5c
PiciAkk <marci@dbx.hu>
2021-11-13 19:48:56 +0100

    időjárás csomag szerkesztve

--- idojaras/modul.py
+++ idojaras/modul.py
@@ -9,8 +9,14 @@ def getCity():
 
 parancs = self.parancs
 beszed = self.beszed
-if "időjárás" in parancs.lower():
-    city = getCity()
+h = self.h
+hangFelismeres = self.hangFelismeres
+
+if "időjárás" in parancs.lower() or "hőmérséklet" in parancs.lower() or "hány fok van" in parancs.lower():
+    if "milyen az időjárás" == parancs.lower() or "milyen a hőmérséklet" == parancs.lower() or "hány fok van" == parancs.lower():
+        city = getCity()
+    else:
+        city = h.stem(parancs.split()[-1])[0]
 
     owm = pyowm.OWM("bc12083e70d2d22298c2df1cec7101d9")
     mgr = owm.weather_manager()
