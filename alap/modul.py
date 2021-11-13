952c67026c546e7ceb066742bb36662b3be6f7ba
PiciAkk <marci@dbx.hu>
2021-11-13 19:51:31 +0100

    Update modul.py

--- alap/modul.py
+++ alap/modul.py
@@ -1,5 +1,6 @@
 parancs = self.parancs
 beszed = self.beszed
+
 if "szia" in parancs.lower() or "hello" in parancs.lower() or "köszöntelek" in parancs.lower() or "helló" in parancs.lower():
     beszed("Köszöntelek! ROBIN vagyok!")
     quit()
