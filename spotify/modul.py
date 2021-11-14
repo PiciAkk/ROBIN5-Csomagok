99c8b6f1e660e8c82a1ea5df69a96ef89cf41592
PiciAkk <marci@dbx.hu>
2021-11-14 19:02:29 +0100

    Update modul.py

--- spotify/modul.py
+++ spotify/modul.py
@@ -13,3 +13,11 @@ elif "indítsd el" in parancs.lower() or "játssz le" in parancs.lower() or "rak
     os.system("spotifycli --play")
     beszed("Zene sikeresen elindítva!")
     quit()
+elif "ugorj a következő" in parancs.lower():
+    os.system("spotifycli --next")
+    beszed("Következő zene!")
+    quit()
+elif "ugorj az előző" in parancs.lower():
+    os.system("spotifycli --prev")
+    beszed("Előző zene!")
+    quit()
