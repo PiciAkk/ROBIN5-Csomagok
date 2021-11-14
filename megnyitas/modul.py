b943a9b5945d0e218a84e32ceacd3e8ecf6ecd0f
PiciAkk <marci@dbx.hu>
2021-11-14 19:24:23 +0100

    Update modul.py

--- megnyitas/modul.py
+++ megnyitas/modul.py
@@ -1,6 +1,15 @@
+import os
+
 beszed = self.beszed
 parancs = self.parancs
 h = self.h
 hangFelismeres = self.hangFelismeres
 
-if "nyisd" in parancs.lower():
+if "nyisd meg" in parancs.lower():
+    alkalmazas = h.stem(parancs.split()[-1])[0]
+    if alkalmazas == "terminál":
+        os.system("xfce4-terminal")
+    else:
+        os.system(alkalmazas)
+    beszed(f"{alkalmazas} sikeresen megnyitva")
+    quit()
